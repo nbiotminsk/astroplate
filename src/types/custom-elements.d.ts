4b1a89da48fa80290397d7c7be8332a5ead09b88
Николай <joomlaplugin@gmail.com>
2026-04-26 12:57:34 +0300

    docs: update warranty from 24 to 12 months

---
+++ src/types/custom-elements.d.ts
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'lite-youtube': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement> & {
+        videoid: string;
+        playlabel?: string;
+        params?: string;
+      }, HTMLElement>;
+    }
+  }
+}
